3d9ad36aae6af40df0d016d535f3e6058ba43ddf
Antonio Bountouvas <abountouvas@gmail.com>
2018-01-27 10:47:45 -0500

    Boy Art Layer Fix

--- Game/FAST/Assets/Scripts/PlayerController.cs
+++ Game/FAST/Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
 
 	public float speed = 20.0f;
