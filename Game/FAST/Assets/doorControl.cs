5c7f4b2400866c1f0b5fb8abbd1d4e63f361af11
Suyi Diao <suyi.diao@vanderbilt.edu>
2018-01-26 21:56:53 -0500

    envirnment setup

---
+++ Game/FAST/Assets/doorControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorControl : MonoBehaviour
+{
+	public int maxCodeNeeded = 3;
+	int codeEntered = 0;
+	bool startLifting = true;
+	float liftTimer = 2f;
+
+	public void codeEnterCorrectly ()
+	{
+		codeEntered++;
+		if (codeEntered == maxCodeNeeded) {
+			startLifting = true;
+		}
+	}
+
+	void Update ()
+	{
+		if (startLifting) {
+			liftTimer -= Time.fixedDeltaTime;
+			if (liftTimer <= 0f) {
+				startLifting = false;
+				liftTimer = 1f;
+			}
+			transform.Translate (Vector2.up * Time.deltaTime);
+		}
+	}
+}
